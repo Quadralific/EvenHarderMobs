46613da97d22a74c89d91efef07767bb3f8d6146
RedstoneNotPlace <79918051+RedstoneNotPlaced@users.noreply.github.com>
2021-11-09 14:25:06 +0800

    Added more powerful mobs
    I made ender, ravanger, phantom and drowned much more powerful

--- src/main/java/me/redstonenotplaced/evenhardermobs/events/SpawnEvent.java
+++ src/main/java/me/redstonenotplaced/evenhardermobs/events/SpawnEvent.java
@@ -4,10 +4,7 @@ import me.redstonenotplaced.evenhardermobs.manager.MobManager;
 import me.redstonenotplaced.evenhardermobs.state.MobState;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Creeper;
-import org.bukkit.entity.Skeleton;
-import org.bukkit.entity.Spider;
-import org.bukkit.entity.Zombie;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
@@ -22,20 +19,27 @@ public class SpawnEvent implements Listener {
         this.mobManager = manager;
     }
 
-    //TODO: I plan to add more mobs
+    //TODO: I plan to add more mobs powerful.
     @EventHandler
     public void onSpawn(CreatureSpawnEvent e) {
         if(mobManager.getMobState() == MobState.ENABLED) {
             switch(e.getEntityType()) {
                 case ZOMBIE:
-                    mobManager.giveArmor((Zombie) e.getEntity(), Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE,
+                    Zombie zombie = (Zombie) e.getEntity();
+                    ItemStack diamondAxe = new ItemStack(Material.DIAMOND_AXE);
+                    mobManager.giveArmor(zombie, Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE,
                             Material.DIAMOND_LEGGINGS, Material.DIAMOND_BOOTS);
-                    mobManager.giveWeapon((Zombie) e.getEntity(), new ItemStack(Material.DIAMOND_AXE), Enchantment.DAMAGE_ALL, 5);
+                    diamondAxe.addEnchantment(Enchantment.DAMAGE_ALL, 1);
+                    mobManager.giveWeapon(zombie, diamondAxe);
                     break;
                 case SKELETON:
-                    mobManager.giveArmor((Skeleton) e.getEntity(), Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE,
+                    Skeleton skeleton = (Skeleton) e.getEntity();
+                    ItemStack bow = new ItemStack(Material.BOW);
+                    mobManager.giveArmor(skeleton, Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE,
                             Material.DIAMOND_LEGGINGS, Material.DIAMOND_BOOTS);
-                    mobManager.giveWeapon((Skeleton) e.getEntity(), new ItemStack(Material.BOW), Enchantment.ARROW_KNOCKBACK, 2);
+                    bow.addEnchantment(Enchantment.ARROW_KNOCKBACK, 2);
+                    bow.addEnchantment(Enchantment.ARROW_FIRE, 1);
+                    mobManager.giveWeapon(skeleton, bow);
                     break;
                 case CREEPER:
                     Creeper creeper = (Creeper) e.getEntity();
@@ -44,8 +48,30 @@ public class SpawnEvent implements Listener {
                     break;
                 case SPIDER:
                     Spider spider = (Spider) e.getEntity();
-                    spider.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999, 4, true, true));
-                    spider.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 999999, 4, true, true));
+                    spider.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999, 4, true, false));
+                    spider.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 999999, 4, true, false));
+                    break;
+                case ENDERMAN:
+                    Enderman enderman = (Enderman) e.getEntity();
+                    enderman.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 999999, 4, true, false));
+                    break;
+                case DROWNED:
+                    Drowned drowned = (Drowned) e.getEntity();
+                    ItemStack trident = new ItemStack(Material.TRIDENT);
+                    mobManager.giveArmor(drowned, Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE, Material.DIAMOND_LEGGINGS,
+                            Material.DIAMOND_BOOTS);
+                    trident.addEnchantment(Enchantment.IMPALING, 5);
+                    mobManager.giveWeapon(drowned, trident);
+                    break;
+                case RAVAGER:
+                    Ravager ravager = (Ravager) e.getEntity();
+                    ravager.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 999999, 9, true, false));
+                    break;
+                case PHANTOM:
+                    Phantom phantom = (Phantom) e.getEntity();
+                    phantom.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 999999, 5, true, false));
+                    phantom.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999, 5, true, false));
+                    phantom.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 999999, 5, true, false));
                     break;
             }
         }
